28aa52e82115163a1fbf3a3477ec8bf458733503
Cyrille DEGASQUET <pro.cyrille.degasquet@gmail.com>
2025-09-11 19:20:26 +0200

    Prisma configuré, seed fonctionnelle

---
+++ prisma.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'prisma/config';
+import path from 'node:path';
+
+export default defineConfig({
+  schema: path.join('prisma', 'schema.prisma'),
+  migrations: {
+    path: path.join('prisma', 'migrations'),
+    seed: 'tsx prisma/seed.ts',
+  },
+});
