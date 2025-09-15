c6d61b0bec1264895b42e4766f47280e05ec2f1f
Cyrille DE GASQUET <pro.cyrille.degasquet@gmail.com>
2025-09-16 01:40:57 +0200

    Débug de la plante index

--- apps/plant-shop-angular-universal/src/app/cart/cart.service.ts
+++ apps/plant-shop-angular-universal/src/app/cart/cart.service.ts
@@ -1,5 +1,6 @@
 // # Importations
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 export type CartItem = {
   plantId: number;
@@ -12,6 +13,7 @@ export type CartItem = {
 export class CartService {
   private storageKey = 'cart_items';
   private items: CartItem[] = [];
+  cartCount$ = new BehaviorSubject<number>(0);
 
   constructor() {
     this.load();
@@ -26,18 +28,21 @@ export class CartService {
       this.items.push(item);
     }
     this.save();
+		this.updateCount();
   }
 
   /** Supprimer une plante du panier */
   remove(plantId: number) {
     this.items = this.items.filter((i) => i.plantId !== plantId);
     this.save();
+		this.updateCount();
   }
 
   /** Vider le panier */
   clear() {
     this.items = [];
     this.save();
+		this.updateCount();
   }
 
   /** Récupérer le contenu du panier */
@@ -54,5 +59,14 @@ export class CartService {
   /** Sauvegarder dans localStorage */
   private save() {
     localStorage.setItem(this.storageKey, JSON.stringify(this.items));
+  }
+
+	/** Mettre à jour le nombre d'articles du panier */
+  private updateCount() {
+    const total = Object.values(this.items).reduce(
+      (sum, item: any) => sum + item.quantity,
+      0
+    );
+    this.cartCount$.next(total);
   }
 }
--- apps/plant-shop-angular-universal/src/app/navbar/navbar.component.ts
+++ apps/plant-shop-angular-universal/src/app/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 import { CommonModule } from '@angular/common';
+import { CartService } from '../cart/cart.service';
 
 // # Composant Navbar
 @Component({
@@ -15,6 +16,15 @@ import { CommonModule } from '@angular/common';
 export class NavbarComponent {
   private auth = inject(AuthService);
   private router = inject(Router);
+  private cartService = inject(CartService);
+
+  nombreArticles = 0;
+
+  constructor() {
+    this.cartService.cartCount$.subscribe((count) => {
+      this.nombreArticles = count;
+    });
+  }
 
   get estConnecte(): boolean {
     return this.auth.isAuthenticated();
