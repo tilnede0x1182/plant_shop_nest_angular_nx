297974c321114ef2a686b41dea397df392143cdd
Cyrille DEGASQUET <pro.cyrille.degasquet@gmail.com>
2025-09-17 04:33:01 +0200

    Débug du token

--- apps/plant-shop-angular-universal/src/app/app.config.ts
+++ apps/plant-shop-angular-universal/src/app/app.config.ts
@@ -1,9 +1,14 @@
+// # Importations
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { appRoutes as routes } from './app.routes';
+import { authInterceptor } from './auth/auth.interceptor';
 
+// # Config application
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
 };
